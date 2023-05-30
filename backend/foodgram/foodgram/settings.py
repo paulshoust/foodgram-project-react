68bbfcdff1be69af8c1a1e8d3321619e03d8627f
Pavel Shoust <paul.shoust@gmail.com>
2023-05-30 22:29:38 +0300

    typo in settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
 Database
-https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+https://docs.djangoproject.com/en/2.2/ref/settings/databases
 
 DATABASES = {
     'default': {
