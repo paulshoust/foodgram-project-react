11c1037298c353dcba671ea093533f675eb0c2dc
Pavel Shoust <paul.shoust@gmail.com>
2023-05-30 21:44:13 +0300

    db new

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -91,6 +91,13 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
