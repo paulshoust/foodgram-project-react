42c6c4b1a5be206814c2ca67bf9a369187c388aa
Pavel Shoust <paul.shoust@gmail.com>
2023-06-04 03:38:07 +0300

    1111

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -3,7 +3,6 @@ from recipe.models import IngredientPerRecipe, Ingredients, Recipes, Tag
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
